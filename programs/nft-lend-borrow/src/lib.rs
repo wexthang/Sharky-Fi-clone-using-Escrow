bdc6072117f23fa69cda180646bd47a846116239
wexthang <166706499+wexthang@users.noreply.github.com>
2024-04-11 19:56:04 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("7Jz69z5dWYNbwEwaM2emTJ1phhBf1zNgLfdBFaBq84H");
 
 #[program]
 pub mod nft_lend_borrow {
